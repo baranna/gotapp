ebab4cf3325c3fd2a4049be35d876916a1156189
baranna <12345>
2019-04-16 15:49:27 +0200

    Add imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CharacterFilterComponent } from './filter/character-filter.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MatButtonModule } from '@angular/material';
+
+
+const appRoutes: Routes = [
+  { path: 'character', component: CharacterFilterComponent },
+  { path: '', component: HomeComponent },
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CharacterFilterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
