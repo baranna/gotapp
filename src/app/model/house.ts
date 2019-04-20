a0e5d49029c5ca57fa110870d75f55319456baa6
baranna <12345>
2019-04-20 15:03:29 +0200

    Add model classes

---
+++ src/app/model/house.ts
@@ -0,0 +1,19 @@
+export class House {
+  constructor(public	url:	string,
+              public	name:	string,
+              public	region:	string,
+              public	coatOfArms:	string,
+              public	words:	string,
+              public	titles:	string[],
+              public	seats:	string[],
+              public	currentLord:	string,
+              public	heir:	string,
+              public	overlord:	string,
+              public	founded:	string,
+              public	founder:	string,
+              public	diedOut:	string,
+              public	ancestralWeapons:	string[],
+              public	cadetBranches:	string[],
+              public	swornMembers:	string[],
+) {}
+}
