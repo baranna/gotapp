c6b6da6fa41ff61cad64d0d0d4da724c1998872a
baranna <12345>
2019-04-23 15:08:37 +0200

    Add House-details page

---
+++ src/app/house-details/house-details.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HouseDetailsComponent } from './house-details/house-details.component';
+
+@NgModule({
+  declarations: [HouseDetailsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HouseDetailsModule { }
