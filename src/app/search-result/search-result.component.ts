2b31e75f30e3bd0d07b92cbf66eaf514a27d0c04
baranna <12345>
2019-04-23 15:11:02 +0200

    Navigating to details pages

--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SearchService} from '../services/search.service';
+import {Observable} from 'rxjs';
+import {Entity} from '../model/entity';
+import {Character} from '../model/character';
+import {Book} from '../model/book';
+import {House} from '../model/house';
 
 @Component({
   selector: 'app-search-result',
   templateUrl: './search-result.component.html',
   styleUrls: ['./search-result.component.css']
 })
+// TODO részletes infók
+// TODO navigálás
+
 export class SearchResultComponent implements OnInit {
 
-  constructor() { }
+  public result: Entity[];
+  public resultCount: number;
+  public detailsLink: string;
+
+  constructor(private searchService: SearchService) {
+  }
 
   ngOnInit() {
+    this.searchService.search().subscribe(
+      result => {
+        this.result = result;
+        this.resultCount = this.result.length;
+        this.detailsLink = this.searchService.type + 'details';
+      }
+    );
+
   }
 
 }
--- src/app/search-result/search-result.module.ts
+++ src/app/search-result/search-result.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchResultComponent } from './search-result.component';
+import {MatCardModule, MatExpansionModule, MatListModule} from '@angular/material';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [SearchResultComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatExpansionModule,
+    MatListModule,
+    MatCardModule,
+    RouterModule
   ]
 })
 export class SearchResultModule { }
