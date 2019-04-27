fe9384cb3fa2db99212ec1400a30e351fd906112
baranna <12345>
2019-04-27 23:11:15 +0200

    Add id-s to result entities

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,8 +1,11 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable} from 'rxjs';
 import {Entity} from '../model/entity';
 import {Character} from '../model/character';
+import {Observable} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
+import {House} from '../model/house';
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +15,7 @@ export class SearchService {
   constructor(private http: HttpClient) {
   }
 
+  // FIXME: redirect a home-ra ha ezek undefined
   public type: string = 'characters';
   public name: string = 'Jon Snow';
 
@@ -19,12 +23,52 @@ export class SearchService {
     this.type = type;
     this.name = name;
   }
+
   search(): Observable<Entity[]> {
-    return this.http.get<Entity[]>('https://anapioficeandfire.com/api/' + this.type + '?name=' + this.name);
+    return this.http.get<Entity[]>(`https://anapioficeandfire.com/api/${this.type}?name=${this.name}`).pipe(
+      map<Entity[], any>(response => {
+        response.forEach( entity => entity.id = entity.url.split('/')[5]);
+        return  response;
+      }
+    ));
   }
 
-  getByUrl(url: string): Observable<any> {
-    return this.http.get<any>(url);
+  getById(entityType: string, id: string): Observable<any> {
+    return this.http.get<any>(`https://anapioficeandfire.com/api/${entityType}/${id}`).pipe(
+      map<Entity, any>(response => {
+        response.id = response.url.split('/')[5];
+        return response;
+      })
+    );
   }
 
+  getCharacterbyId(id: string): Observable<Character> {
+    return this.http.get<Character>(`https://anapioficeandfire.com/api/characters/${id}`).pipe(
+      map<Character, any>(
+        response => {
+
+        var spouseUrl = <string><unknown>response.spouse;
+        if (spouseUrl) {
+          this.getById("characters", spouseUrl.split('/')[5]).subscribe(
+            result => response.spouse = result
+          );
+        }
+
+        for (let i = 0; i < response.allegiances.length; i++) {
+          let id = (<string><unknown>response.allegiances[i]).split('/')[5];
+          this.getById('houses', id).subscribe( result => {
+            response.allegiances[i] = result;
+            response.allegiances[i].id = id;
+          });
+        }
+
+        response.id = response.url.split('/')[5];
+        return response;
+      })
+    );
+  }
+
+  getHouseId(id: string): Observable<House> {
+    return this.http.get<House>(`https://anapioficeandfire.com/api/houses/${id}`);
+  }
 }
