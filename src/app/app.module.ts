88c551350d70386b4eb1d16922b16ef6109d34a0
baranna <12345>
2019-05-04 18:35:31 +0200

    Refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { RouterModule, Routes } from '@angular/router';
-import { MatButtonModule } from '@angular/material';
 import {HomeModule} from './home/home.module';
-import {MatFormFieldModule} from '@angular/material/form-field';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {SearchResultComponent} from './search-result/search-result.component';
 import {SearchResultModule} from './search-result/search-result.module';
 import {HttpClientModule} from '@angular/common/http';
 import {SearchService} from './services/search.service';
-import {CharacterDetailsComponent} from './character-details/character-details/character-details.component';
 import {CharacterDetailsModule} from './character-details/character-details.module';
-
-import {HouseDetailsComponent} from './house-details/house-details/house-details.component';
-
 import {HouseDetailsModule} from './house-details/house-details.module';
-import {BookDetailsComponent} from './book-details/book-details/book-details.component';
 import {BookDetailsModule} from './book-details/book-details.module';
 import { AppRoutingModule } from './app-routing.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+//TODO: KOMMENTEZÉS
 
 @NgModule({
   declarations: [
@@ -30,9 +21,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   ],
   imports: [
     BrowserModule,
-    MatButtonModule,
     HomeModule,
-    MatFormFieldModule,
     BrowserAnimationsModule,
     SearchResultModule,
     HttpClientModule,
