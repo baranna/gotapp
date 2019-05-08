4de7ba53bc367a84b815e7e863cadff6a00376d7
baranna <12345>
2019-05-08 18:58:17 +0200

    Make page not found page

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
@@ -7,9 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageNotFoundComponent implements OnInit {
 
-  //TODO: page not found
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
